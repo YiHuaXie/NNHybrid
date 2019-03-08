9848a928e28d322f1424310c4e6632b3df118f9f
NeroXie <xyh30902@163.com>
2019-03-08 14:39:05 +0800

    iOS pod install

---
+++ NNHybridiOS/NNHybridiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NNHybridiOS
+//
+//  Created by NeroXie on 2019/3/8.
+//  Copyright © 2019 NeroXie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
