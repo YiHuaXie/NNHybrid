6c4c18334cdb8cc326bd9c764ca0f898fd01fcc9
NeroXie <xyh30902@163.com>
2019-03-09 22:35:42 +0800

    add redux for rn

--- index.js
+++ index.js
@@ -5,8 +5,6 @@
 
 import { AppRegistry } from 'react-native';
 import App from './App';
-import App2 from './App2';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent('App1', () => App);
-AppRegistry.registerComponent('App2', () => App2);
+AppRegistry.registerComponent(appName, () => App);
