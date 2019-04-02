0c1f2ddfa382c5c0ffb014a1508ce01ac418c551
NeroXie <xyh30902@163.com>
2019-04-02 23:29:52 +0800

    code update

---
+++ NNHybridRN/components/common/EachHouseCell.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { 
+    StyleSheet,
+    View,
+    Image,
+    Text,
+} from 'react-native';
+
+export default class EachHouseCell extends Component {
+    
+    render() {
+        return(
+            <View style={styles.container}>
+
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        
+    },
+});
