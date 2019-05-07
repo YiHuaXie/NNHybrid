3fff0cca9fa728ef3482c3f606ae9bb5c7ea07e3
NeroXie <xyh30902@163.com>
2019-05-07 17:32:14 +0800

    code upda te

---
+++ NNHybridiOS/NNHybridiOS/RNBridge/RCTView/MapLocationViewManager.h
@@ -0,0 +1,17 @@
+//
+//  MapLocationViewManager.h
+//  NNHybridiOS
+//
+//  Created by NeroXie on 2019/5/7.
+//  Copyright © 2019 NeroXie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MapLocationViewManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ NNHybridiOS/NNHybridiOS/RNBridge/RCTView/MapLocationViewManager.m
@@ -0,0 +1,13 @@
+//
+//  MapLocationViewManager.m
+//  NNHybridiOS
+//
+//  Created by NeroXie on 2019/5/7.
+//  Copyright © 2019 NeroXie. All rights reserved.
+//
+
+#import "MapLocationViewManager.h"
+
+@implementation MapLocationViewManager
+
+@end
